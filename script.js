8f2240ffa3902e74c8204e460d103a990aa74017
baletso <design.letb@gmail.com>
2025-06-18 13:31:01 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const imageUrls = [
-    "URL_DE_TU_IMAGEN_1.jpg",
-    "URL_DE_TU_IMAGEN_2.png",
+    "https://drive.google.com/file/d/1UxPOClcYE8Jv-4fY8rDOmHQY0XzI9HJT/view",
+    "https://drive.google.com/file/d/1AbMU4RJE2zPtifj2m2gOQfGBL1TVC2T_/view",
     "URL_DE_TU_IMAGEN_3.jpeg",
     // Agrega aquí todas las URLs de tus imágenes, cada una entre comillas y separada por coma
     "URL_DE_TU_IMAGEN_N.gif"
