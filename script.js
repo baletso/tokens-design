6c769cc4c5bf64c8526f4c2f71f02252369c2eed
baletso <design.letb@gmail.com>
2025-06-18 13:37:56 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const imageUrls = [
-    "https://drive.google.com/file/d/1UxPOClcYE8Jv-4fY8rDOmHQY0XzI9HJT/view",
-    "https://drive.google.com/file/d/1AbMU4RJE2zPtifj2m2gOQfGBL1TVC2T_/view",
-    "URL_DE_TU_IMAGEN_3.jpeg",
-    // Agrega aquí todas las URLs de tus imágenes, cada una entre comillas y separada por coma
-    "URL_DE_TU_IMAGEN_N.gif"
+    "semillas/tarjeta-verde.png",
+    "semillas/tarjeta-verde.png",
+    "semillas/tarjeta-verde.png",
+    "semillas/tarjeta-verde.png",
+    "semillas/tarjeta-verde.png",
+    "semillas/tarjeta-verde.png",
+    "semillas/tarjeta-verde.png",
 ];
 
 function displayRandomImage() {
